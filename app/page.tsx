81f9913f43d0695bf291507ef7d5b69c4fea2960
jjeffrin <jjeffrin@gmail.com>
2025-01-05 12:07:01 +0530

    added comments

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,22 @@ import { cn } from "@/lib/utils";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-[100vh]">
+
+      {/* Image */}
       <img src="img/Houses-amico.svg" className="h-[15rem]" />
+
+      {/* Heading */}
       <h1 className="font-bold text-3xl text-center mb-1">One Brick Architects</h1>
+
+      {/* Sub Content */}
       <p className="text-center text-sm mb-6">Peak performance architecting, it's as good as it gets.</p>
       <div
         className={cn(
           "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800",
         )}
       >
+
+        {/* See More Button */}
         <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
           <span className="text-sm">✨ See our work</span>
           <FaArrowDown className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
